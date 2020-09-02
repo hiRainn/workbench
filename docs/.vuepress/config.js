d8d3422d1501421294d8668e3e9b177adf4f086a
hiRainn <wr_sorarara@yahoo.co.jp>
2020-09-02 21:31:51 +0800

    docs config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,32 @@ module.exports = {
 	title: 'workbench',
 	description: 'Just playing around',
 	base: '/',
+	
 	markdown: {
-		lineNumbers: true
+	  lineNumbers: true
+	},
+	themeConfig:{
+		repo: 'hirainn/workbench',
+		docsDir: 'docs',
+		editLinks: false,
+		editLinkText: '在 Github 上帮助我们编辑此页',
+		lastUpdated: 'Last Updated',
+		smoothScroll: true,
+		sidebar:[
+			{
+			  title: '简介',
+			  collapsable: false,
+			  children: [
+				  {title:'为什么创建',path:'/why/reason'}
+			  ]
+			},
+			{
+			  title: '开始',
+			  collapsable: false,
+			  children: [
+			    '/start/start', '/start/use'
+			  ]
+			},
+		]
 	}
 }
---
+++ docs/start/use.md
@@ -0,0 +1,12 @@
+---
+title:快速使用
+lang:zh_CN
+---
+
+# 使用 
+
+仅仅需要简单的设置配置 
+
+```vue 
+vue.use(component)
+```
