897db87e9c6fb9c37ca9e567d0ed7caa7a8dc786
hiRainn <wr_sorarara@yahoo.co.jp>
2020-09-25 16:45:04 +0800

    singal

--- api/main.go
+++ api/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
+	"syscall"
 	"workbench/model"
 	"workbench/route"
 )
@@ -21,9 +24,20 @@ func main() {
 	if config.Port == "" {
 		config.Port = "8080"
 	}
-
 	model.InitDB()
 	car := route.Initroute()
 	car.Use(gin.Logger())
+
+	//监听打断信号
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGQUIT, os.Interrupt)
+	go func() {
+		sig := <-sigs
+		fmt.Println(sig)
+		db := model.GetDb()
+		db.Close()
+		os.Exit(0)
+	}()
+
 	car.Run(":" + config.Port)
 }
