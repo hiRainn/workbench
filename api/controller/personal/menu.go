8632f361309b49974a2cbf27745516e56106a246
hiRainn <wr_sorarara@yahoo.co.jp>
2020-09-29 17:49:19 +0800

    update

---
+++ api/controller/personal/menu.go
@@ -0,0 +1,20 @@
+package personal
+
+import "github.com/gin-gonic/gin"
+
+//获取菜单
+func GetMenu(c *gin.Context) {
+
+}
+
+func AddMenu(c *gin.Context) {
+
+}
+
+func EditMenu(c *gin.Context) {
+
+}
+
+func DelMenu(c *gin.Context) {
+
+}
--- api/route/personal/route.go
+++ api/route/personal/route.go
@@ -1,9 +1,17 @@
 package personal
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	p "workbench/controller/personal"
+)
 
 func InitPersonalRoute() *gin.Engine {
 	r := gin.Default()
+	//menu
+	r.GET("menu", p.GetMenu)
+	r.POST("menu", p.AddMenu)
+	r.PUT("menu/:id", p.EditMenu)
+	r.DELETE("menu/:id", p.DelMenu)
 
 	return r
 
