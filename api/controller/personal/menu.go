ae7881ae0f6a282b921fabc3b9a8a532e780d3ad
hiRainn <wr_sorarara@yahoo.co.jp>
2020-10-02 21:37:13 +0800

    add

--- api/controller/personal/menu.go
+++ api/controller/personal/menu.go
@@ -2,12 +2,24 @@ package personal
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"workbench/model/personal"
 )
 
+type MenuList struct {
+	Path     string    `json:"path"`
+	Name     string    `json:"name"`
+	Meta     string    `json:"meta"`
+	Children *MenuList `json:"children"`
+}
+
 //获取菜单
 func GetMenu(c *gin.Context) {
-	personal.GetMenu()
+	list, err := personal.GetMenu()
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusOK, list)
 }
 
 func AddMenu(c *gin.Context) {
@@ -19,5 +31,9 @@ func EditMenu(c *gin.Context) {
 }
 
 func DelMenu(c *gin.Context) {
+
+}
+
+func FormatMenu() {
 
 }
